b084efbe79d2338f5897cdb09be79579c82edcf5
INFO2-PC14\3D Tarde <tarancodelgadocarlos@gmail.com>
2018-03-05 18:08:28 +0100

    Botones salir con confirmación funcionando

--- Assets/Scripts/Salir.cs
+++ Assets/Scripts/Salir.cs
@@ -1,13 +1,44 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Salir : MonoBehaviour {
 
-    public void Exit()
+    [SerializeField] GameObject panelSalir;
+    public GameObject image;
+    public Button button;
+
+    void Start()
+    {
+        button.onClick.AddListener(() => {
+            image.SetActive(true);
+        });
+    }
+
+
+//public void OnSalir_Click()
+//    {
+//        if (panelSalir.activeSelf == false)
+//            {
+//            panelSalir.SetActive(true);
+//            }
+//            //panelSalir.SetActive(true);
+//    }
+
+        public void OnAceptar_Click()
     {
         Debug.Log("SALIR");
         Application.Quit();
+    }
+    public void OnCancelar_Click()
+    {
+
+        if (panelSalir.activeSelf == true)
+           {
+          panelSalir.SetActive(false);
+          }
+
     }
 
 }
