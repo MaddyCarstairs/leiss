c8d3e67ae05fa70ae7f8a4153f5228ff23e29639
ElenaJolie <35272409+ElenaJolie@users.noreply.github.com>
2018-03-07 18:56:28 +0100

    intro lista

---
+++ Assets/Scripts/controlador.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class controlador : MonoBehaviour {
+
+    public void CambiarEscena(string login)
+    {
+        print("Cambiando a la escena" + login);
+        SceneManager.LoadScene(login);
+    }
+
+    public void Salir()
+    {
+        print("Saliendo del juego");
+        Application.Quit();
+
+    }
+}
